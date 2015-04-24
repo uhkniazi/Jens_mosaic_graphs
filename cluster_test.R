c72392f7ec532a35f81b51c52c8b01d6fc22f243
uhkniazi <uhkniazi@users.noreply.github.com>
2015-04-24 15:44:29 +0100

    2 types of heatmaps - one containing all genes, other with mean expression in each cluster

--- cluster_test.R
+++ cluster_test.R
@@ -84,18 +84,12 @@ f = V(oIGbp)$type
 n = V(oIGbp)[f]$name
 V(oIGbp)[n]$logFC = dfDat[n,'Fold.change']
 V(oIGbp)[n]$symbol = dfDat[n,'X']
-# get the neighbours i.e. reactome ids for each enterez id
-for (i in 1:length(n)){
-  nv = neighbors(oIGbp, V(oIGbp)[n[i]])
-  V(oIGbp)[n[i]]$Reactome = list(V(oIGbp)[nv]$name)
-}
 
 ## graph projection to one dimension
 # create the CGraph object and calculate obs to exp weights after projection
 obj = CGraph(oIGbp)
 # create a projection of the graph 
 oIGProj = CGraph.getProjectedGraph(obj)
-#rm(obj)
 
 ## some genes are orphans as they don't share reactome terms with others, remove those
 d = degree(oIGProj)
@@ -153,48 +147,48 @@ c = grep('Flu\\.Pos|Flu\\.Neg|Healthy', c)
 c = grep('Flu\\.Pos|Flu\\.Neg', c)
 dfCounts.fp = dfCounts[,c]
 
-mCounts = t(dfCounts.fp)
-mDist = as.matrix(dist(t(mCounts)))
-# create graph of this matrix 
-oIGdist = graph.adjacency(mDist, mode='min', weighted=T)
-# remove gene connections that are farther apart form each other
-w = E(oIGdist)$weight
-summary(w)
-par(p.old)
-hist(w)
-# choose a cutoff by modelling the distribution shape
-# it appears that the distribution follows a power law?
-# taking square root means we can fit a poisson distribution
-w2 = sqrt(w)
-r = range(w2)
-s = seq(floor(r[1])-0.5, ceiling(r[2])+0.5, by = 1)
-hist(w2, prob=T, breaks=s, main='distribution of eucledian distances', 
-     xlab='square root of distances', ylab='')
-r = round(r)
-dp = dpois(r[1]:r[2], lambda = median(w2))
-dn = dnbinom(r[1]:r[2], size = median(w2), mu = median(w2))
-lines(r[1]:r[2], dp, col='red', type='b')
-lines(r[1]:r[2], dn, col='blue', type='b')
-legend('topright', legend = c('poi', 'nbin'), fill = c('red', 'blue'))
-
-# NOTE: this cutoff can be changed, the lower it is the less edges in the graph
-# use poisson/negative binomial to choose cutoff
-#c = qnbinom(0.05, size = median(w2), mu=median(w2), lower.tail = F)
-c = qpois(0.05, median(w2), lower.tail = F)
-f = which(w2 >= c)
-oIGdist = delete.edges(oIGdist, edges = f)
-# get the distance matrix again / adjacency matrix
-mDist = as.matrix(get.adjacency(oIGdist, 'both', attr = 'weight', names = T))
-# invert the weights, things that are farthest will have the smallest number
-# others that are closest to each other will have the most remaining behind
-# e.g. 10 - 5 = 5 no change in weight
-# 10 - 8 = 2 as these 2 are far away, they will have a weight of 2 remaining etc
-mDist = max(mDist) - mDist
-# no connections or weights on diagonals
-diag(mDist) = 0  
-# create the graph of distances/correlations
-oIGdist = graph.adjacency(mDist, mode='min', weighted=T)
-E(oIGdist)$distance = E(oIGdist)$weight
+# mCounts = t(dfCounts.fp)
+# mDist = as.matrix(dist(t(mCounts)))
+# # create graph of this matrix 
+# oIGdist = graph.adjacency(mDist, mode='min', weighted=T)
+# # remove gene connections that are farther apart form each other
+# w = E(oIGdist)$weight
+# summary(w)
+# par(p.old)
+# hist(w)
+# # choose a cutoff by modelling the distribution shape
+# # it appears that the distribution follows a power law?
+# # taking square root means we can fit a poisson distribution
+# w2 = sqrt(w)
+# r = range(w2)
+# s = seq(floor(r[1])-0.5, ceiling(r[2])+0.5, by = 1)
+# hist(w2, prob=T, breaks=s, main='distribution of eucledian distances', 
+#      xlab='square root of distances', ylab='')
+# r = round(r)
+# dp = dpois(r[1]:r[2], lambda = median(w2))
+# dn = dnbinom(r[1]:r[2], size = median(w2), mu = median(w2))
+# lines(r[1]:r[2], dp, col='red', type='b')
+# lines(r[1]:r[2], dn, col='blue', type='b')
+# legend('topright', legend = c('poi', 'nbin'), fill = c('red', 'blue'))
+# 
+# # NOTE: this cutoff can be changed, the lower it is the less edges in the graph
+# # use poisson/negative binomial to choose cutoff
+# #c = qnbinom(0.05, size = median(w2), mu=median(w2), lower.tail = F)
+# c = qpois(0.05, median(w2), lower.tail = F)
+# f = which(w2 >= c)
+# oIGdist = delete.edges(oIGdist, edges = f)
+# # get the distance matrix again / adjacency matrix
+# mDist = as.matrix(get.adjacency(oIGdist, 'both', attr = 'weight', names = T))
+# # invert the weights, things that are farthest will have the smallest number
+# # others that are closest to each other will have the most remaining behind
+# # e.g. 10 - 5 = 5 no change in weight
+# # 10 - 8 = 2 as these 2 are far away, they will have a weight of 2 remaining etc
+# mDist = max(mDist) - mDist
+# # no connections or weights on diagonals
+# diag(mDist) = 0  
+# # create the graph of distances/correlations
+# oIGdist = graph.adjacency(mDist, mode='min', weighted=T)
+# E(oIGdist)$distance = E(oIGdist)$weight
 
 ## use correlation matrix to create graph
 mCounts = t(dfCounts.fp)
@@ -209,32 +203,17 @@ par(p.old)
 hist(c)
 summary(c)
 E(oIGcor)$cor = E(oIGcor)$weight
-# t = abs(c)
-# hist(t, prob=T)
-# # the correlation looks fairly normal
-# dn = dnorm(seq(0, 1, 0.01), mean(t), sd(t))
-# lines(seq(0, 1, 0.01), dn)
-# summary(t)
-# dn = dnorm(seq(0, 1, 0.01), median(t), mad(t))
-# lines(seq(0, 1, 0.01), dn, col='red')
-# # choose appropriate cutoff based on data
-# f = which((c >= -0.5 & c <= 0.5))
-# # if we need genes that are not correlated
-# f = which((c <= -0.5 | c >= 0.5))
 
 # keep only positively correlated genes connected
 f = which(c < 0.5)
 oIGcor = delete.edges(oIGcor, edges = f)
 
 ### graph intersection
-# intersect the 3 graphs
-l = list(oIGProj, oIGcor, oIGdist)
+l = list(oIGProj, oIGcor)
+
 ig.1 = graph.intersection(l)
 # set observed to expected ratio as weight
 E(ig.1)$weight = E(ig.1)$ob_to_ex
-# color the positive and negative edges differently
-# col = ifelse(E(ig.1)$weight_2 < 0, 'red', 'black')
-# E(ig.1)$color = col
 
 par(mar=c(1,1,1,1))
 plot(ig.1, vertex.label=NA, vertex.size=1, layout=layout.fruchterman.reingold, vertex.frame.color=NA)
@@ -252,8 +231,71 @@ dend = as.dendrogram(com)
 col = brewer.pal(9, 'Greens')
 n = V(ig.2)$name
 mCounts.heat = t(mCounts[,colnames(mCounts) %in% n])
+c = colnames(mCounts.heat)
+c = gsub('^X.+(Pos|Neg|Healthy).+', '\\1', c)
+colnames(mCounts.heat) = c
+mCounts.heat = mCounts.heat[,order(colnames(mCounts.heat))]
 par(p.old)
-heatmap(mCounts.heat, Rowv = dend, scale='row', col=col, labCol = NA, margins = c(0.5,0.5))
+heatmap(mCounts.heat, Rowv = dend, scale='row', col=col, cexRow = 0.2, cexCol = 0.5), Colv = NA)
+
+## cut the tree of communities into sub groups and find the reactome 
+## pathways associated with those genes. this is a multi step process
+
+# recreate the bipartite graph but only with nodes that are in our final graph
+oIGbp = CGraph.getBipartiteGraph(obj)
+# get the indices for the vertices of type 1, i.e. genes
+f = V(oIGbp)$type
+n = V(oIGbp)[f]$name
+# get names of genes present in last graph after cutoffs
+n2 = V(ig.2)$name
+# intersect the names
+i = !(n %in% n2)
+# choose the names not commong between bipartite graph and the last graph
+n = n[i]
+oIGbp = delete.vertices(oIGbp, v = n)
+d = degree(oIGbp)
+# delete orphan nodes
+oIGbp = delete.vertices(oIGbp, which(d == 0))
+f = V(oIGbp)$type
+
+hc = as.hclust(com)
+plot(hc)
+memb = membership(com)
+memb = cutree(hc, h = 230)
+table(memb)
+mCent = matrix(NA, nrow = length(unique(memb)), ncol = ncol(mCounts.heat))
+rv = rep(NA, length=nrow(mCent))
+rv.g = rep(NA, length=nrow(mCounts.heat))
+# get the centers and reactome terms for these genes from each cluster
+for (i in 1:length(unique(memb))){
+  # check if cluster has one member only
+  if (sum(memb == i) == 1) {
+    mCent[i,] = mCounts.heat[memb == i,]
+  } else {
+  # else if more than one member, we can use mean 
+  mCent[i,] = colMeans(mCounts.heat[memb == i,])}
+  rn = rownames(mCounts.heat)[memb == i]
+  # get the reactome pathway names
+  nei = graph.neighborhood(oIGbp, order = 1, nodes = rn)
+  pw = sapply(seq_along(nei), function(x) V(nei[[x]])$name)
+  pw = unlist(pw)
+  pw = as.data.frame(table(pw))
+  rv[i] = as.character(pw[which.max(pw$Freq), 1])
+  rv.g[memb == i] = rv[i]
+}
+rownames(mCent) = rv
+mCounts.heat.2 = mCounts.heat
+rownames(mCounts.heat.2) = rv.g
+dend = hc
+dend$labels = rv.g
+dend = as.dendrogram(dend)
+heatmap(mCounts.heat.2, Rowv = dend, scale='row', col=col, cexRow = 0.1, cexCol = 0.3, Colv = NA)
+
+# heatmap based on means of each community/cluster
+colnames(mCent) = colnames(mCounts.heat)
+hc.2 = hclust(dist(mCent), members = table(memb))#, method = 'cent')
+heatmap(mCent, Rowv = as.dendrogram(hc.2), scale='row', col=col, cexRow = 0.5, cexCol = 0.3, Colv = NA)
+heatmap(log(mCent), Rowv = as.dendrogram(hc.2), scale='row', col=col, cexRow = 0.5, cexCol = 0.5), Colv = NA)
 
 # recreate the bipartite graph but only with nodes that are in our final graph
 oIGbp = CGraph.getBipartiteGraph(obj)
